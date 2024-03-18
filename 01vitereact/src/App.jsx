1ad2b663f15aa7dc1a60a36baf3342c5eba88a86
Vaishnavi <viasnavigupta2320@gmail.com>
2024-03-18 16:12:33 +0530

    React

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,11 +2,12 @@ import Learn from "./Learn"
 
 function App() {
   
-
+const username="chai aur code"
   return (
     <>
     <Learn />
-   <h1>first react lecture</h1>
+   <h1>first react lecture{username}</h1>
+   //{}- this is evaluated expression i.e final outcome
    <p>this is me -para</p>
   </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+const anotherElement=(
+    <a href="https://google.com" target='_blank'>Visit google </a>
+)
 
+const reactElement= React.createElement(
+    'a',
+    {href:'https://google.com', target: '_blank'},
+    'click me to visit'
+
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
  
-    <App />
-  
+   // <App />
+   //anotherElement
+  reactElement
 )
